7648adca8d388d73bf23d5cb8b8990bba3c444f4
Tayyiba kaisar <tayyibakaisar@Tayyibas-MBP.fios-router.home>
2023-01-14 10:55:26 -0500

    Class1

---
+++ Modules/BasicLinuxcommands.js
@@ -0,0 +1,21 @@
+/**
+    Basic Linux commands:
+
+    1. pwd	-> Present working Directory
+
+    2. ls -alrt	-> to view the content of folder/directory
+    (Any file/directory name starts with a dot ; means it's hidden)
+
+    3. cd -> Change Directory
+    to go onto a folder/directory
+
+    4. mkdir -> Make Directory
+    to create a folder in local
+
+    5. rmdir -> Remove Directory
+    to delete an empty folder in local
+
+    6. rm -rf -> Remove directory forcefully
+    to delete a folder in local
+
+ */
---
+++ Modules/comments.js
@@ -0,0 +1,26 @@
+// anything after the double-slash is going to be consider as comment for that line.
+// double-slash can be used to provide a single line comment.
+
+/*
+    this is 
+
+    a way to 
+
+    add multi-line
+
+    comment in js
+
+*/
+
+/**
+ * this is
+ * 
+ * another way
+ * 
+ * to add multi-line
+ * 
+ * 
+ * comments in js
+ */
+
+ var num = 10;
